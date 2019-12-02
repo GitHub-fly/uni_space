4075448c980693fe323c21df35ea871db68b21b8
GitHub-fly <1224178565@qq.com>
2019-12-02 08:59:34 +0800

    新建远程仓库

---
+++ src/main/java/com/scs/web/uni_space/UniSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.scs.web.uni_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scs/web/uni_space/UniSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.web.uni_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
